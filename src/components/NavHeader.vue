8e4a5353210249abbc8cbbf59425d7c85b40fd0d
yuanwei <yuanwei@kexinjinke.com>
2021-05-13 16:27:03 +0800

    NavHeader组件的导航菜单实现

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -20,7 +20,33 @@
 		</div>
 		
 		<!--头部组件导航条-->
-		<div class="nav-header"></div>
+		<div class="nav-header">
+			<div class="container">
+				<div class="header-logo">
+					<a href="/#/index"></a>
+				</div>
+				<div class="header-menu">
+					<div class="item-menu">
+						<span>小米手机</span>
+						<div class="children"></div>
+					</div>
+					<div class="item-menu">
+						<span>RedMi手机</span>
+						<div class="children"></div>
+					</div>
+					<div class="item-menu">
+						<span>电视</span>
+						<div class="children"></div>
+					</div>
+				</div>
+				<div class="header-search">
+					<div class="wrapper">
+						<input type="text" name="keyword">
+						<a href="javascipt:;"></a>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -31,6 +57,7 @@
 </script>
 
 <style lang="scss">
+	@import '../assets/scss/base';
 	.header{
 		.nav-topbar{
 			height: 39px;
@@ -38,9 +65,6 @@
 			background-color: #333333;
 			color: #B0b0B0;
 			.container{
-				width:1226px;
-				margin-left: auto;
-				margin-right: auto;
 				display: flex;
 				justify-content: space-between;
 				align-items: center;
@@ -61,6 +85,90 @@
 						background: url('/imgs/icon-cart-checked.png') no-repeat center;
 						background-size: contain;
 						margin-right: 4px;
+					}
+				}
+			}
+		}
+		.nav-header{
+			.container{
+				height: 112px;
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
+				.header-logo{
+					display: inline-block;
+					width: 55px;
+					height: 55px;
+					background-color: #FF6600;
+					a{
+						display: inline-block;
+						width: 110px;
+						height: 55px;
+						&:before{
+							content: '';
+							display: inline-block;
+							width: 55px;
+							height: 55px;
+							background: url('/imgs/mi-logo.png') no-repeat center;
+							background-size: contain;
+							transition: margin .2s;
+						}
+						&:after{
+							content: '';
+							display: inline-block;
+							width: 55px;
+							height: 55px;
+							background: url('/imgs/mi-home.png') no-repeat center;
+							background-size: contain;
+						}
+						&:hover::before{
+							margin-left: -55px;
+							transition: margin .2s;
+						}
+					}
+				}
+				.header-menu{
+					display: inline-block;
+					width: 643px;
+					padding-left: 209px;
+					.item-menu{
+						display: inline-block;
+						color: #333333;
+						font-weight: bold;
+						font-size: 16px;
+						line-height: 112px;
+						margin-right: 20px;
+						span{
+							cursor:pointer;
+						}
+						&:hover{
+
+						}
+					}
+				}
+				.header-search{
+					width: 319px;
+					.wrapper{
+						height: 50px;
+						border: 1px solid #E0E0E0;
+						display: flex;
+						align-items: center;
+						input {
+							border: none;
+							box-sizing: border-box;
+							border-right: 1px solid #E0E0E0;
+							width: 264px;
+							height: 50px;
+							padding-left: 14px;
+						}
+						a{
+							display: inline-block;
+							width: 18px;
+							height: 18px;
+							background: url('/imgs/icon-search.png') no-repeat center;
+							background-size: contain;
+							margin-left: 17px;
+						}
 					}
 				}
 			}
